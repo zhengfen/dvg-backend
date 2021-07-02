6b2476c05a5df1ec07e1273a722d86bec802dc7c
Fen <tozhengfen@gmail.com>
2021-07-02 10:23:34 +0200

    use postgresql

--- backend/settings.py
+++ backend/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'blogs', 
+        'USER' : 'blogs', 
+        'PASSWORD' : 'blogs', 
+        'HOST' : 'localhost', 
+        'PORT' : '5432'
     }
 }
 
