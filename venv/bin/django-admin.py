f2adfb79fae7ebf1c9f451638513cae4883a9c8d
Fen <tozhengfen@gmail.com>
2021-07-03 15:29:24 +0200

    put virtual env files in venv

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/fen/Projects/django_projects/dvg/backend/bin/python
+#!/home/fen/Projects/django_projects/django-vue-graphQL-blogs/backend/venv/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
